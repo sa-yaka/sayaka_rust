a448edfb88f152d17ad6fe8877da94ceb322a48e
sa-yaka <sx.sayaka@gmail.com>
2022-06-03 11:17:42 +0800

    struct

---
+++ data_type/struct_type/target/debug/deps/struct_type-57a5f7dc45c9108f.d
@@ -0,0 +1,5 @@
+/home/sayaka/Projects/sayaka_rust/data_type/struct_type/target/debug/deps/struct_type-57a5f7dc45c9108f.rmeta: src/main.rs
+
+/home/sayaka/Projects/sayaka_rust/data_type/struct_type/target/debug/deps/struct_type-57a5f7dc45c9108f.d: src/main.rs
+
+src/main.rs:
